86c4321274fb6f31f7a7b03fb1c5df48c43af605
Tolga Takır <tolga@thundra.io>
2021-08-09 23:29:07 +0300

    Change app flow

--- src/main/java/io/thundra/demo/localstack/handler/Archive.java
+++ src/main/java/io/thundra/demo/localstack/handler/Archive.java
@@ -2,33 +2,36 @@ package io.thundra.demo.localstack.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import io.thundra.demo.localstack.service.AppRequestService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Archive implements RequestHandler<Map<String, String>, Map<String, String>> {
+public class Archive implements RequestHandler<SNSEvent, Void> {
     private static final Logger logger = LogManager.getLogger(Archive.class);
     private AppRequestService appRequestService = new AppRequestService();
 
     @Override
-    public Map<String,String> handleRequest(Map<String,String> request, Context context) {
-        try {
-            logger.info("Archive Handle Request -->" + request);
-            String requestId = appRequestService.getRequestId(request);
-            //put result onto S3
-            appRequestService.archiveAppRequest(requestId);
-            //simulate processing delay
-            Thread.sleep(TimeUnit.SECONDS.toMillis(3));
-            //set request status to FINISHED
-            appRequestService.addAppRequest(requestId, "FINISHED");
-            return request;
-        } catch (IOException | InterruptedException e) {
-            logger.error("Error occurred handling message. Exception is ", e);
-            throw new RuntimeException(e);
-        }
+    public Void handleRequest(SNSEvent request, Context context) {
+        logger.info("Archive Handle Request -->" + request);
+        List<SNSEvent.SNSRecord> records = request.getRecords();
+        records.forEach(snsRecord -> {
+            try {
+                String requestId = appRequestService.getRequestId(snsRecord.getSNS().getMessage());
+                //put result onto S3
+                appRequestService.archiveAppRequest(requestId);
+                //simulate processing delay
+                Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+                //set request status to FINISHED
+                appRequestService.addAppRequest(requestId, "FINISHED");
+            } catch (IOException | InterruptedException e) {
+                logger.error("Error occurred handling message. Exception is ", e);
+            }
+        });
+        return null;
     }
 }
